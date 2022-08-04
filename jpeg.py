1491cbe305834cf439a6b602feeb2259dea6b93c
fahcsim <mischatravis10@gmail.com>
2022-08-03 18:00:02 -0700

    various updates

--- jpeg.py
+++ jpeg.py
@@ -19,24 +19,30 @@ from create_db_sqlite import create_db_sqlite
 con = sqlite3.connect('data.db', isolation_level=None)
 cur = con.cursor()
 
+def check_directories(directory):
+  if not os.path.exists(directory):
+    os.mkdir(directory)
+  if not os.path.exists(f"{directory}/tmp"):
+    os.mkdir(f"{directory}/tmp")
+
 def grab_jpeg(directory,camera_friendly,shinobi_ip,api_key,group_key,camera_id):
   now = timestamp.now()
   filename = (directory + camera_friendly + now + '.jpeg')
+  filename_tmp = (directory + 'tmp/' + camera_friendly + now + '.jpeg')
   imgURL = ('http://' + shinobi_ip + '/' + api_key + '/jpeg/' + group_key + '/' + camera_id + '/s.jpg')
   try:
-      urllib.request.urlretrieve(imgURL, filename)
-      return filename, now
+      urllib.request.urlretrieve(imgURL, filename_tmp)
+      return filename, filename_tmp, now
   except:
       logging.info("no response from Shinobi, waiting 10 seconds. If the app is just starting up, this is fine to ignore")
       sleep(10)
       self.now = timestamp.now()
       filename = (directory + camera_friendly + now + '.jpeg')
+      filename_tmp = (directory + '/tmp/' + camera_friendly + now + '.jpeg')
       urllib.request.urlretrieve(imgURL, filename)
       print(filename)
-      return filename, now
+      return filename, filename_tmp, now
 
-
-########################################################
 def draw_objects(draw, objs, labels):
   """Draws the bounding box and label for each object."""
   for obj in objs:
@@ -65,13 +71,13 @@ def main():
       labels = data["labels"]
       threshold = data["threshold"]
       count = data["count"]
+  check_directories(directory)
   shinobi_image = grab_jpeg(directory,camera_friendly,shinobi_ip,api_key,group_key,camera_id)
-  print(shinobi_image[0])
   labels = read_label_file(labels) if labels else {}
   interpreter = make_interpreter(model)
   interpreter.allocate_tensors()
 
-  image = Image.open(shinobi_image[0])
+  image = Image.open(shinobi_image[1])
   _, scale = common.set_resized_input(
       interpreter, image.size, lambda size: image.resize(size, Image.ANTIALIAS))
 
@@ -84,6 +90,7 @@ def main():
   if not objs:
     success = False
     print('No objects detected')
+    os.remove(shinobi_image[1])
   else:
     success = True
 
@@ -95,16 +102,23 @@ def main():
       ymin = obj.bbox.ymin
       label = (labels.get(obj.id))
       filename = shinobi_image[0]
-      now = shinobi_image[1]
+      filename_tmp = shinobi_image[1]
+      now = shinobi_image[2]
       detection = {'predictions': [{'x_max': xmax, 'x_min': xmin, 'y_max': ymax,  'y_min': ymin, 'label': label, 'confidence': confidence }], 'success': success}
       
       print(detection)
       image = image.convert('RGB')
       draw_objects(ImageDraw.Draw(image), objs, labels)
-      image.save(shinobi_image[0])
+      image.save(filename)
+      os.remove(filename_tmp)
     ## Insert values into database
       cur.execute("INSERT INTO DETECTIONS(LABEL, CONFIDENCE, Y_MIN, Y_MAX, X_MIN, X_MAX, CAMERA_ID, TIMESTAMP, FILENAME) VALUES (?,?,?,?,?,?,?,?,?)", (thing, confidence, ymin, ymax, xmin, xmax, camera_friendly, now, filename))
       con.commit()
+      
 while True:
   if __name__ == '__main__':
     main()
+    time.sleep(5)
+
+## todo
+# make sure photo/tmp is added automatically
